4248e894519e60b0cb91c85e11976eb78d8be1e1
mfronterhouse <mallory.fronterhouse@mykenzie.snhu.edu>
2023-03-14 12:23:07 -0400

    design changes made

--- login.js
+++
@@ -1,4 +0,0 @@
-import {MDCTextField} from '@material/textfield';
-
-const username = new MDCTextField(document.querySelector('.username'));
-const password = new MDCTextField(document.querySelector('.password'));
